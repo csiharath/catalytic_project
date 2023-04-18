5fc9f3a95ef8fa0640ccdeaf1f5f69123e9f1d62
JoPkb <jeremiemprokob@gmail.com>
2023-04-18 16:51:38 +0200

    Prédictions de KM et Kcat

--- code/predict_kcat_model.py
+++ code/predict_kcat_model.py
@@ -1,16 +1,17 @@
 import pickle
-import torch
-import pandas as pd
-from KM_prediction import KM_predicton
+from kcat_prediction import kcat_predicton
 
-model, alphabet = torch.hub.load("facebookresearch/esm:v0.4.0", "esm1b_t33_650M_UR50S")
-
-with open("data/km_enzyme.p", "rb") as e:
+with open("data/kcat_enzyme.p", "rb") as e:
     enzymes = pickle.load(e)
 
-with open("data/km_substrat.p", "rb") as s:
-    substrats = pickle.load(s)
+with open("data/kcat_substrat.p", "rb") as s:
+    substrates = pickle.load(s)
 
+with open("data/kcat_product.p", "rb") as p:
+    products = pickle.load(p)
 
-df_km = KM_predicton(substrate_list = substrats, enzyme_list= enzymes)
-df_km.to_csv("predicted_km.csv")
+df_kcat = kcat_predicton(substrates = substrates,
+               products = products,
+               enzymes = enzymes)
+
+df_kcat.to_csv("predicted_km.csv")
